bccd2e30e331a30744e74d96ee3c4307e2f51b92
Sasa Tunde Femi <35744541+sasquare@users.noreply.github.com>
2025-05-31 21:36:46 +0100

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://fidelbridge-backend.onrender.com" // your deployed backend URL
-    : "http://localhost:5000";
+  process.env.REACT_APP_SOCKET_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://fidelbridge-backend.onrender.com"
+    : "http://localhost:5000");
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket"], // prefer websocket
+  transports: ["websocket"],
   withCredentials: true,
 });
 
