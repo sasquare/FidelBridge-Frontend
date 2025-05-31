9d3f53b5aa8cf315d6e002b8dac5a9d81aadbd3a
Sasa Tunde Femi <35744541+sasquare@users.noreply.github.com>
2025-05-31 20:40:26 +0100

    Update ProfessionalProfile.js

--- src/pages/ProfessionalProfile.js
+++ src/pages/ProfessionalProfile.js
@@ -13,7 +13,9 @@ import "../components/profile/Profile.css";
 const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:5000";
 const socket = io(SOCKET_URL, {
   transports: ["websocket", "polling"],
+  withCredentials: true,
 });
+
 
 function ProfessionalProfile() {
   const { id } = useParams();
