dc95614516dcc9babbc095df4c936783d17da52f
Sasa Tunde Femi <35744541+sasquare@users.noreply.github.com>
2025-05-31 02:37:15 +0100

    Update axiosConfig.js

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL:
+    process.env.REACT_APP_API_URL || "https://servicehub-zxrdgmbv.b4a.run",
   timeout: 10000,
   withCredentials: true,
 });
