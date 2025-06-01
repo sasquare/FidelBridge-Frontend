3b06648facfedc4d7eb3a67e2830f20488dccb7c
Sasa Tunde Femi <35744541+sasquare@users.noreply.github.com>
2025-06-01 16:30:53 +0100

    Update axiosConfig.js

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://fidelbridge-backend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
   timeout: 10000,
   withCredentials: true,
 });
